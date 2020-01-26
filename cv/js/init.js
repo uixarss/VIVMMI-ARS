de256ad9984e22445231c6357a89f9683b5a9377
Apriansyah Rizqi Setiawan <48873366+xprnsyh@users.noreply.github.com>
2020-01-26 12:53:45 +0700

    Add files via upload

---
+++ cv/js/init.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.tooltipped').tooltip();
+
+    /* Scroll */
+    var btn = $('#scroll');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+});
